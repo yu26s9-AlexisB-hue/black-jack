66b8c55a1cf1197fa88cd8b2363a37ec3a47dfcc
Alexis Barrett <happynessb09@gmail.com>
2026-05-06 16:28:41 -0400

    finished the hand class

--- src/main/java/com/pluralsight/Hand.java
+++ src/main/java/com/pluralsight/Hand.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Hand {
+    private ArrayList<Cards> cards = new ArrayList<>();
+
+    public void dealInto(Cards card){
+        cards.add(card);
+    }
+
+    public int getSize(){
+        return cards.size();
+    }
+
+    public int getValue(){
+        int value = 0;
+
+        for(Cards card: cards){
+            if(card.isFaceUp()){
+                value += card.getPointValue();
+            }else{
+                card.flip(); //turn the card over to see the value
+                value += card.getPointValue();
+                card.flip(); // hide the card again
+            }
+        }
+        return value;
+    }
 }
